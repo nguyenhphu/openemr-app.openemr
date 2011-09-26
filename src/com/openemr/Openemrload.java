cfdb78ca4a2fdfd87d38406626b3200d45962448
astrollawayfrommadness <astrollawayfrommadness@gmail.com>
2011-09-26 16:05:44 -0500

    work

--- src/com/openemr/Openemrload.java
+++ src/com/openemr/Openemrload.java
@@ -1,10 +1,6 @@
 package com.openemr;
 
-<<<<<<< HEAD
 
-=======
-//push from eclipse
->>>>>>> refs/remotes/origin/master
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
