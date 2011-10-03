8872ce93c0de53ecdd6a1cce9d8fd749b8907ba6
astrollawayfrommadness <astrollawayfrommadness@jgmail.com>
2011-10-03 19:08:07 -0400

    all location recources entered, starting on navigation UI

--- gen/com/openemr/R.java
+++ gen/com/openemr/R.java
@@ -17,14 +17,31 @@ public final class R {
         public static final int logo1=0x7f020003;
     }
     public static final class id {
-        public static final int button1=0x7f060005;
-        public static final int button2=0x7f060006;
-        public static final int button3=0x7f060007;
-        public static final int fragment1=0x7f060009;
+        public static final int button1=0x7f060004;
+        public static final int button10=0x7f06000d;
+        public static final int button11=0x7f06000e;
+        public static final int button12=0x7f06000f;
+        public static final int button13=0x7f060010;
+        public static final int button14=0x7f060011;
+        public static final int button15=0x7f060012;
+        public static final int button16=0x7f060013;
+        public static final int button17=0x7f060014;
+        public static final int button18=0x7f060015;
+        public static final int button19=0x7f060016;
+        public static final int button2=0x7f060005;
+        public static final int button20=0x7f060017;
+        public static final int button21=0x7f060018;
+        public static final int button3=0x7f060006;
+        public static final int button4=0x7f060007;
+        public static final int button5=0x7f060008;
+        public static final int button6=0x7f060009;
+        public static final int button7=0x7f06000a;
+        public static final int button8=0x7f06000b;
+        public static final int button9=0x7f06000c;
+        public static final int fragment1=0x7f06001a;
         public static final int handle=0x7f060002;
-        public static final int linearLayout1=0x7f060003;
-        public static final int linearLayout2=0x7f060004;
-        public static final int settings=0x7f060008;
+        public static final int linearLayout2=0x7f060003;
+        public static final int settings=0x7f060019;
         public static final int slidingDrawer=0x7f060001;
         public static final int webview0=0x7f060000;
     }
--- src/com/openemr/Browser.java
+++ src/com/openemr/Browser.java
@@ -9,10 +9,12 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 /**
  * Reusable class for creating fully funtional webviews.
  * @author arimal
@@ -26,6 +28,9 @@ public class Browser extends Activity
 	WebView webview;
 	SharedPreferences preferences;
 	final Activity activity = this; //shorthand assignments
+	
+	    
+	
 	@Override//override annotations allow us to create our own functionality for the any methods of the super class
     public void onCreate(Bundle savedInstanceState) //all innitations happen here in onCreate
 	{
@@ -34,12 +39,215 @@ public class Browser extends Activity
         this.getWindow().requestFeature(Window.FEATURE_PROGRESS);
         setContentView(R.layout.main);
         this.load(R.id.webview0, "/openemr");
+  
+        
+        
+        final Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrLeftNav));
+            }
+        });
+        
+        
+        final Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrNewPatient));
+            }
+        });
+ 
+        final Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrHtmNavigation));
+            }
+        });
+        
+        
+        final Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrMessages));
+            }
+        });
+        
+        final Button button5 = (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrCalendarOnly));
+            }
+        });
+        
+        final Button button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrOfficeNotes));
+            }
+        });
+        
+        final Button button7 = (Button) findViewById(R.id.button7);
+        button7.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrAddressBook));
+            }
+        });
+        
+        final Button button8 = (Button) findViewById(R.id.button8);
+        button8.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrPatientEncounterForm));
+            }
+        });
+ 
+        
+        final Button button9 = (Button) findViewById(R.id.button9);
+        button9.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrCreateWorkSchoolNotes));
+            }
+        });
+ 
+        final Button button10 = (Button) findViewById(R.id.button10);
+        button10.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrNewDictation));
+            }
+        });
+ 
+        final Button button11 = (Button) findViewById(R.id.button11);
+        button11.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrViewDictation));
+            }
+        });
+ 
+        final Button button12 = (Button) findViewById(R.id.button12);
+        button12.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrOrderProcedure));
+            }
+        });
+ 
+        final Button button13 = (Button) findViewById(R.id.button13);
+        button13.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrViewChangeProcedure));
+            }
+        });
+ 
+        final Button button14 = (Button) findViewById(R.id.button14);
+        button14.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrReviewBodySystemsROS));
+            }
+        });
+ 
+        final Button button15 = (Button) findViewById(R.id.button15);
+        button15.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrReviewOfBodySystemsChecked));
+            }
+        });
+ 
+        final Button button16 = (Button) findViewById(R.id.button16);
+        button16.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrReviewPatientVitalStats));
+            }
+        });
+ 
+        final Button button17 = (Button) findViewById(R.id.button17);
+        button17.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrCreatePatientVitalStatsInstance));
+            }
+        });
+ 
+        final Button button18 = (Button) findViewById(R.id.button18);
+        button18.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrOrdersResults));
+            }
+        });
+ 
+        final Button button19 = (Button) findViewById(R.id.button19);
+        button19.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrReportsIndex));
+            }
+        });
+ 
+        final Button button20 = (Button) findViewById(R.id.button20);
+        button20.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrPendingFollowup));
+            }
+        });
+ 
+        final Button button21 = (Button) findViewById(R.id.button21);
+        button21.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+            	load(R.id.webview0, getString(R.string.OpenemrPendingOrders));
+            }
+        });
+ 
+        
+        
+        
+        
+        
+        
         
 	  //  webview.loadUrl(preferences.getString("IP", getString(R.string.srv))+"/openemr");
 	   //	this.load(R.id.webview1, "/openemr");
        //webview.loadUrl(preferences.getString("IP", getString(R.string.srv))+"/openemr");
        
-	}   	   	
+        
+	} 
+	
 	@Override //handle back button event
 	public boolean onKeyDown(int keyCode, KeyEvent event) 
 	{ 
